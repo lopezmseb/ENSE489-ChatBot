f4e69028ad7006b6d722dee64160159e6564f1f6
Sebastian Lopez <90015639+lopezmseb@users.noreply.github.com>
2023-07-15 00:33:26 -0600

    Object Oriented ChatBot

---
+++ Chatbot/ui_helper.py
@@ -0,0 +1,16 @@
+import sys
+
+bot_name = "Bot: "
+
+def speak(message):
+    print(f"{bot_name}: {message}")
+
+def query(message):
+    request = ""
+    while(request == ""):
+        request = input(message)
+
+    if(request.lower() == "exit" or request.lower() == "quit"):
+        sys.exit(0)
+
+    return request
