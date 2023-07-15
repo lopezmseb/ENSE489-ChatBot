4088f9c25800f733b0e45d3c91085c2da054d411
Sebastian Lopez <90015639+lopezmseb@users.noreply.github.com>
2023-07-15 00:47:43 -0600

    proper try catch

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,36 +1,37 @@
 from finance_chatbot import FinanceChatbot
 from ui_helper import query
 import sys
-import os
 import json
 
 file_path = "user_info.json"
 load_model = True if "-l" in sys.argv else False
-name = "User"
+name = ""
 
 def wrap_text(name, text):
     return name + ": " + text
 
 
-# if (not load_model):
-#     res = input("Load model from file? (Y/N) ")
-#     if (res.lower() == "y"):
-#         load_model = True
-#     else:
-#         load_model = False
-#
-# with open(file_path, "w+") as file:
-#     try:
-#         data = json.load(file)
-#     except json.JSONDecodeError:
-#         data = {}
-#
-#     if (os.path.getsize(file_path) == 0):
-#         name = input("What is your name? ")
-#         data["name"] = name
-#         json.dump(data, file, indent=4)
-#     else:
-#         name = data.get('name')
+if (not load_model):
+    res = input("Load model from file? (Y/N) ")
+    if (res.lower() == "y"):
+        load_model = True
+    else:
+        load_model = False
+
+with open(file_path, "w+") as file:
+    try:
+        data = json.load(file)
+    except json.JSONDecodeError:
+        data = {}
+
+    try:
+        name = data.get('name')
+    except(KeyError):
+        name = input("What is your name? ")
+        data["name"] = name
+
+        json.dump(data, file, indent=4)
+
 
 assistant = FinanceChatbot(load_model)
 assistant.welcome_message()
